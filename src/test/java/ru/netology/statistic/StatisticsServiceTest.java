ce2c01f6aae229c82e29b17fad685e2acf786a17
Julia Hlimoneko <707010@gmail.com>
2023-01-11 20:55:16 +0200

    added test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,17 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findEquality()  {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {2, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
 }
